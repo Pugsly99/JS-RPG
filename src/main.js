e1e06472464bbf4ec1a1e4b879ed9a783bcfdc57
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-03 11:41:22 -0800

    Add select character feature
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-
+import { playerSelect, storeState, stateControl, changeState, canDoThings, intro} from './js/character.js';
 $;
+storeState;
+stateControl;
+changeState;
+canDoThings;
+
+
+$("#startGame").submit(function(event) {
+  event.preventDefault();
+  const characterClass = intro();
+  console.log(characterClass);
+  characterClass;
+  const currentPlayer = playerSelect(characterClass);
+  console.log(currentPlayer);
+
+});
