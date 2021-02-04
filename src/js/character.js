52920311816621a66eb0577b255770cffd04982b
Benjamin McFarland <benrmcfarland@gmail.com>
2021-02-03 16:07:13 -0800

    Add health change function, and inventory
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>
    Co-authored-by: Taylor Delph <taylulzcode@gmail.com>

--- src/js/character.js
+++ src/js/character.js
@@ -1,43 +1,7 @@
-const Player = { 
-  class:"", 
-  health:"", 
-  inventory:[],
-  sick:false,
-};
-
-Player;
-
-export const intro = () => {
-  console.log("Choose who you are during this pandemic!");
-  console.log("Child -All chances at getting covid reduced by 50%, less access to resources- (1)");
-  console.log("Athlete -If catches covid, 50% of not turning into zombie, less access to resources- (2)");
-  console.log("Elderly -All chances of getting covid increased by 50%, more access to resources- (3)");
-  const input = prompt("Who are you?");
-  if (input === "1" || input === "2" || input === "3") {
-    return input;
-  } else {
-    console.log("Invalid input! Try agian.");
-    return intro();
-  }
-};
-
-export const playerSelect = (characterClass) => {
-  if(characterClass === "1"){
-    const initialChild = ({class: "Child", health: 30, sick: false, inventory: []});
-    return initialChild;
-  }else if (characterClass === "2"){
-    const initialAthlete = ({class: "Athlete", health: 90, sick: false, inventory: []});
-    return initialAthlete;
-  }else if (characterClass === "3"){
-    const initialElderly = ({class: "Elderly", health: 50, sick: false, inventory: ["yoohoo", "yoohoo", "tp"]});
-    return initialElderly;
-  }
-};
-
 // This function stores our state.
 
-export const storeState = () => {
-  let currentState = {};
+export const storeState = (initialState) => {
+  let currentState = initialState;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
@@ -70,7 +34,24 @@ export const canDoThings = function(creature) {
   return obj;
 };
 
-const smallTp = changeState("health")(1);
+export const playerSelect = (characterClass) => {
+  if(characterClass === "1"){
+    const initialChild = ({class: "Child", health: 30, sick: false, inventory: []});
+    return initialChild;
+  }else if (characterClass === "2"){
+    const initialAthlete = ({class: "Athlete", health: 90, sick: false, inventory: []});
+    return initialAthlete;
+  }else if (characterClass === "3"){
+    const initialElderly = ({class: "Elderly", health: 50, sick: false, inventory: ["yoohoo", "yoohoo", "tp", "tp"]});
+    return initialElderly;
+  }
+};
+
+// const initialValues = {soil: 0, water: 0, light: 0};
+// const fern = storeState(initialValues);
+// fern;
+
+export const smallTp = changeState("health")(1);
 smallTp;
 const bigTp = changeState("health")(5);
 bigTp;
--- src/main.js
+++ src/main.js
@@ -2,20 +2,69 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { playerSelect, storeState, stateControl, changeState, canDoThings, intro} from './js/character.js';
+import { playerSelect, storeState, stateControl, changeState, canDoThings, smallTp } from './js/character.js';
 $;
 storeState;
 stateControl;
 changeState;
 canDoThings;
 
+function intro() {
+  console.log("Choose who you are during this pandemic!");
+  console.log("Child -All chances at getting covid reduced by 50%, less access to resources- (1)");
+  console.log("Athlete -If catches covid, 50% of not turning into zombie, less access to resources- (2)");
+  console.log("Elderly -All chances of getting covid increased by 50%, more access to resources- (3)");
+  const input = prompt("Who are you? Enter 1 for a child, 2 for an athlete, 3 for an elder");
+  if (input === "1" || input === "2" || input === "3") {
+    return input;
+  } else {
+    console.log("Invalid input! Try agian.");
+    return intro();
+  }
+}
 
-$("#startGame").submit(function(event) {
-  event.preventDefault();
-  const characterClass = intro();
-  console.log(characterClass);
-  characterClass;
-  const currentPlayer = playerSelect(characterClass);
-  console.log(currentPlayer);
+$(document).ready(function () {
+  // event.preventDefault();
+  $("#startGame").click(function () {
+    const currentPlayer = storeState(playerSelect(intro()));
+    console.log(currentPlayer());
+    $("#class-value").text(currentPlayer().class);
+    currentPlayer().inventory.forEach(item => $("#inventory-value").append(`${item}, `));
+    $("#health-value").text(currentPlayer().health);
 
+    $("#addHealth").click(function () {
+      if (currentPlayer().inventory.includes("tp")) {
+        for (let i = -1, y = 0; i < y; y++) {
+          if (currentPlayer().inventory[y] === "tp") {
+            currentPlayer().inventory.splice(i);
+            const newCurrentPlayer = currentPlayer(smallTp);
+            $("#inventory-value").empty();
+            newCurrentPlayer.inventory.forEach(item => $("#inventory-value").append(`${item}, `));
+            $("#health-value").text(newCurrentPlayer.health);
+            i += y + 2;
+            return;
+          }
+        }
+      } else {
+        console.log("Your out of toilet paper!");
+      }
+      // $("#health-value").text(currentPlayer().health);
+      // $("#inventory-value").empty();
+      // currentPlayer().inventory.forEach(item => $("#inventory-value").append(`${item}, `));
+    });
+  });
 });
+
+
+// $('#addHealth').click(function() {
+//   const currentHealth = stateControl();
+//   $('#health-value').text(currentHealth.health);
+// });
+
+// if ((player.strength + (Math.floor(Math.random()* 20) + 1)) > enemy.armorTotal) {
+//   let damageDealt = (player.equippedMeleeWeapon.weaponStrength + (Math.floor(Math.random()* 20) + 1));
+//   enemy.health -= damageDealt;
+//   console.log(`You have dealt the ${enemy.id} ${damageDealt} damage! They appear to have ${enemy.health} health remaining.`);
+// } else {
+//   console.log("You Missed.");
+// }
